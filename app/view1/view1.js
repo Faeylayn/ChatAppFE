35db583827bd3942d3cce803558c2d1d1e895495
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-08-19 19:46:30 -0400

    Updated items from framework

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,10 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+  console.log($scope);
+  $scope.login = function() {
+    console.log($scope.username);
+    alert("hi")
+  }
 }]);
