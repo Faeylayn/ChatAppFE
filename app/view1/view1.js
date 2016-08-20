c634dae1066c8116b8fd8c615a933009a6113364
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-08-19 22:11:31 -0400

    Added logic for login controller and login function

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,17 +9,20 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('View1Ctrl', ['$scope', '$http', '$cookies', '$window',
+  function($scope, $http, $cookies, $window) {
   $scope.login = function() {
     $http({
       method: 'POST',
       url: 'http://localhost:5000/LoginUser',
-      data: {UserName: 'ajdobbasdfs.sm@gmail.com'}
+      data: {UserName: $scope.username}
     }).then(function(response) {
-      console.log(response);
+      // console.log(response);
+      $cookies.put('username', $scope.username)
+      $window.location.href = "/#/view2"
     }, function(error) {
       console.log("error", error);
     })
-    console.log($scope.username);
+    // console.log($scope.username);
   }
 }]);
