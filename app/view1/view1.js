a19676b08cf2b7e1be527813ad00ee75cfbc8b20
Angela Julie Dobbs <ajdobbs.sm@gmail.com>
2016-08-19 20:23:53 -0400

    Got the http working to loginuser on the api

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,10 +9,17 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope) {
-  console.log($scope);
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
   $scope.login = function() {
+    $http({
+      method: 'POST',
+      url: 'http://localhost:5000/LoginUser',
+      data: {UserName: 'ajdobbasdfs.sm@gmail.com'}
+    }).then(function(response) {
+      console.log(response);
+    }, function(error) {
+      console.log("error", error);
+    })
     console.log($scope.username);
-    alert("hi")
   }
 }]);
